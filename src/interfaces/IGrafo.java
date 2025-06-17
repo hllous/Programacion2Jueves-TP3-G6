3fcad55d97d0850f31efb5ef5a2af29a6be3d969
FacuIria <facuiria03@gmail.com>
2025-06-17 17:32:38 -0300

    Actualizo IGrafo.java

--- src/interfaces/IGrafo.java
+++ src/interfaces/IGrafo.java
@@ -1,12 +1,11 @@
 package interfaces;
 import java.util.Map;
-import java.util.List;
 
 public interface IGrafo<T> {
     void agregarNodo(T valor);
     void agregarArista(T origen, T destino, int peso);
     Map<T, INodo<T>> getNodos();
-    void recorridoDFS(T inicio);
-    void recorridoBFS(T inicio);
+    void dfs(T inicio);
+    void bfs(T inicio);
     void matrizDeAdyacencia();
 }
